ddb5d6dfb1d3af6cbe81b2ea9c04dea5b7fff306
Diego Lopez <diegolopez@meta.com>
2025-06-23 23:55:07 -0700

    Project Setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+//Imports
+const express = require("express");
+require("dotenv").config();
+
+//Starting config
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p>Bruh</p>
+    </div>
+  );
+}
+
+export default App;
