d02baa6ed3cae7d20aecd52ce0b343bc424b7e77
Diego Lopez <diegolopez@meta.com>
2025-07-02 15:12:33 -0700

    Commit (Medica History enpoints):     - The user can retrieve the patients whole medical History     - The docotor can create a new register in the patient medical history

--- backend/routes/medicalHistory.js
+++ backend/routes/medicalHistory.js
@@ -4,6 +4,27 @@ const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+//Retrieve all the patients medical records
+router.get("/get-patient-medical-history/:patientId", async (req, res) => {
+  const patientId = parseInt(req.params.patientId);
+
+  try {
+    const patientMedicalHistory = await prisma.medicalHistory.findMany({
+      where: {
+        patientId: patientId,
+      },
+    });
+
+    res.status(200).json({ patientMedicalHistory });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal server error retrieving the medical record",
+      details: error.message,
+    });
+  }
+});
+
+//Create a new register in the medical history
 router.post("/create-medical-history", async (req, res) => {
   const { patientId, doctorId, diagnosis, notes } = req.body;
 
