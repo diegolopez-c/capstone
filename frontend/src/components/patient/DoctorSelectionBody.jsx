643a780ab73f777b82525ca5487c553e730d71ed
Diego Lopez <diegolopez@meta.com>
2025-07-07 22:57:45 -0700

    implementation of the endpoint in the frontend

--- frontend/src/components/patient/DoctorSelectionBody.jsx
+++ frontend/src/components/patient/DoctorSelectionBody.jsx
@@ -1,8 +1,43 @@
-import React, { useState } from "react";
-import { Select, SelectItem, Button, addToast } from "@heroui/react";
+import React, { useEffect, useState } from "react";
+import { Select, SelectItem, Button, addToast, Spinner } from "@heroui/react";
 
 export default function DoctorSelectionBody({ selectDoctor }) {
   const [selectedDoctor, setSelectedDoctor] = useState();
+
+  const [doctorsAvailable, setDoctorsAvailable] = useState([]);
+
+  const [loadingDoctors, setLoadingDoctors] = useState(true);
+
+  useEffect(() => {
+    async function fetchDoctorsAvailable() {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_BASE_URL + "/user/get-doctors-available",
+          {
+            method: "GET",
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch doctors");
+        }
+        const data = await response.json();
+        setDoctorsAvailable(data);
+      } catch (error) {
+        addToast({
+          title: "Error",
+          description: error.message,
+          type: "error",
+        });
+      } finally {
+        setLoadingDoctors(false);
+      }
+    }
+    fetchDoctorsAvailable();
+  }, []);
+
+  if (loadingDoctors) {
+    return <Spinner />;
+  }
 
   return (
     <div className="w-full flex items-center justify-center flex-col gap-6">
@@ -18,15 +53,13 @@ export default function DoctorSelectionBody({ selectDoctor }) {
           setSelectedDoctor(doctor);
         }}
       >
-        <SelectItem className="bg-ca-light-black" key="YO">
-          gurt
-        </SelectItem>
-        <SelectItem className="bg-ca-light-black" key="KANYE">
-          WEST
-        </SelectItem>
-        <SelectItem className="bg-ca-light-black" key="CHINO">
-          PACAS
-        </SelectItem>
+        {doctorsAvailable.map((doc) => {
+          return (
+            <SelectItem className="bg-ca-light-black" key={doc.id}>
+              {doc.name}
+            </SelectItem>
+          );
+        })}
       </Select>
       <Button
         className="bg-ca-yellow"
