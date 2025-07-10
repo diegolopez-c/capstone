b640fd095c2b04da7730266b48b411486cf3eded
Diego Lopez <diegolopez@meta.com>
2025-07-10 16:29:58 -0700

    Frontend Auth Redirection

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Avatar, Button } from "@heroui/react";
+import { Avatar, Button, Spinner } from "@heroui/react";
 import placeholder from "../assets/images/download.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ProfileCard() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
-    <div className="w-full border border-blue-100 h-1/2 flex flex-col items-center justify-center text-center">
-      <div className="w-1/2 min-w-48 border border-white-100 rounded-2xl flex flex-col items-center justify-center text-center gap-3 py-7 bg-ca-white text-ca-black shadow-[0px_4px_6px_0px_rgba(255,_255,_255,_0.05)]">
+    <div className="w-full h-1/2 flex flex-col items-center justify-center text-center">
+      <div className="w-1/2 min-w-48 border-2 border-black bg-ca-black rounded-2xl flex flex-col items-center justify-center text-center gap-3 py-7 text-ca-white shadow-[0px_4px_6px_0px_rgba(255,_255,_255,_0.05)]">
         <Avatar
           className="w-24 h-auto text-ca-mint"
-          isBordered
-          src={placeholder}
+          src={isAuthenticated ? user.picture : "Patient PFP"}
         />
-        <h3>Name</h3>
-        <h3>Speciality</h3>
+        <h3>{isAuthenticated ? user.given_name : "Patient Name"}</h3>
+        <h3>{isAuthenticated ? user.family_name : "Patient Lastname"}</h3>
         <h3>Id</h3>
         <Button className="w-min bg-ca-mint border border-ca-dark-blue">
           View Profile
