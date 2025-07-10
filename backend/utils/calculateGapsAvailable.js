9f35fd8cd1d40a792809c8afc4478cac26dcb716
Diego Lopez <diegolopez@meta.com>
2025-07-10 12:43:48 -0700

    Calculate a doctor schedule Gaps Available

---
+++ backend/utils/calculateGapsAvailable.js
@@ -0,0 +1,28 @@
+const getTimeInMinutes = require("./getTimeInMinutes");
+
+//The function needs the doctorAvailability object and the doctors appointments of that day
+function calculateGapsAvailable(doctorAvailability, doctorAppointments) {
+  let availableHours = [];
+
+  let curHour = getTimeInMinutes(new Date(doctorAvailability.startTime));
+  let shiftEnd = getTimeInMinutes(new Date(doctorAvailability.endTime));
+
+  while (curHour < shiftEnd - 30) {
+    //If theres not an appointment in the 30 min +- from the current hour
+    const overlapAppointments = doctorAppointments.find((app) => {
+      return (
+        Math.abs(curHour - getTimeInMinutes(new Date(app.scheduleDate))) > 30
+      );
+    });
+
+    if (overlapAppointments === undefined) {
+      availableHours.push(curHour);
+    }
+
+    curHour += 30;
+  }
+
+  return availableHours;
+}
+
+module.exports = calculateGapsAvailable;
