83935ce76fd26e5d3894ecd89bbff4a39e440248
Diego Lopez <diegolopez@meta.com>
2025-07-11 12:31:55 -0700

    fixed a mini merge conflict

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -30,15 +30,10 @@ export default function ProfileCard() {
   return (
     <div className="w-full h-1/2 flex flex-col items-center justify-center text-center">
       <div className="w-1/2 min-w-48 border-2 border-black bg-ca-black rounded-2xl flex flex-col items-center justify-center text-center gap-3 py-7 text-ca-white shadow-[0px_4px_6px_0px_rgba(255,_255,_255,_0.05)]">
-    <div className="w-full h-1/2 flex flex-col items-center justify-center text-center">
-      <div className="w-1/2 min-w-48 border-2 border-black bg-ca-black rounded-2xl flex flex-col items-center justify-center text-center gap-3 py-7 text-ca-white shadow-[0px_4px_6px_0px_rgba(255,_255,_255,_0.05)]">
         <Avatar
           className="w-24 h-auto text-ca-mint"
           src={isAuthenticated ? user.picture : "Patient PFP"}
-          src={isAuthenticated ? user.picture : "Patient PFP"}
         />
-        <h3>{isAuthenticated ? user.given_name : "Patient Name"}</h3>
-        <h3>{isAuthenticated ? user.family_name : "Patient Lastname"}</h3>
         <h3>{isAuthenticated ? user.given_name : "Patient Name"}</h3>
         <h3>{isAuthenticated ? user.family_name : "Patient Lastname"}</h3>
         <h3>Id</h3>
