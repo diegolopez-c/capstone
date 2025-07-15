83b160f55b475f6a21b18a610dd3b58c67cef6a8
Diego Lopez <diegolopez@meta.com>
2025-07-15 16:51:24 -0700

    Design minor changes

--- frontend/src/components/doctor/NewPrescriptionMain.jsx
+++ frontend/src/components/doctor/NewPrescriptionMain.jsx
@@ -30,33 +30,35 @@ export default function NewPrescriptionMain() {
   }
 
   return (
-    <div className="w-3/4 h-screen flex flex-col items-center gap-4 py-8">
+    <div className="w-3/4 h-screen flex flex-col items-center gap-10 py-8">
       <h1 className="text-ca-white text-3xl font-bold">
         Make New Prescription
       </h1>
-      <h3 className="text-ca-white text-xl font-bold">
-        Search Patient By ID Number
-      </h3>
-      <div className="w-4/5 gap-4 flex items-center justify-center">
-        <NumberInput
-          hideStepper
-          className="max-w-xs"
-          label="Patient ID"
-          placeholder="Enter the patient id"
-          value={curPatientId}
-          onChange={(e) => {
-            setCurPatientId(e.target.value);
-          }}
-        />
-        <Button
-          onPress={() => {
-            getPatientInfo();
-          }}
-        >
-          Select Patient
-        </Button>
+      <div className="flex flex-col w-full items-center gap-4">
+        <h3 className="text-ca-white text-xl font-bold">
+          Search Patient By ID Number
+        </h3>
+        <div className="w-4/5 gap-4 flex items-center justify-center">
+          <NumberInput
+            hideStepper
+            className="max-w-xs"
+            label="Patient ID"
+            placeholder="Enter the patient id"
+            value={curPatientId}
+            onChange={(e) => {
+              setCurPatientId(e.target.value);
+            }}
+          />
+          <Button
+            onPress={() => {
+              getPatientInfo();
+            }}
+          >
+            Select Patient
+          </Button>
+        </div>
+        <PatientInfo patientBody={selectedPatient} />
       </div>
-      <PatientInfo patientBody={selectedPatient} />
       <div className="w-full flex gap-4 justify-around items-center">
         <PastPrescriptions
           patientId={selectedPatient ? selectedPatient.id : null}
