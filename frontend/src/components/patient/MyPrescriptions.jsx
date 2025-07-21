21d9a7ac4e04b18daa6685767c2c2bdd97276b65
Diego Lopez <diegolopez@meta.com>
2025-07-21 09:03:46 -0700

    Setting up the prescriptions page

---
+++ frontend/src/components/patient/MyPrescriptions.jsx
@@ -0,0 +1,105 @@
+import { useState, useCallback } from "react";
+import { useEffect } from "react";
+import {
+  cancelAppointment,
+  fetchAllPatientAppointments,
+} from "../../api/appointmentFunctions";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Tooltip,
+  Chip,
+  addToast,
+} from "@heroui/react";
+import { columns } from "../../utils/appointmentTableColumns";
+import formatFullDate from "../../utils/formatFullDate";
+
+export default function MyPrescriptions() {
+  const { user, isLoading } = useAuth0();
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  useEffect(() => {
+    async function fetchAppointments(email) {
+      const list = await fetchAllPatientAppointments(email);
+      setAppointmentList(list);
+    }
+
+    if (!isLoading && user) {
+      fetchAppointments(user.email);
+    }
+  }, [isLoading, user]);
+
+  const renderCell = useCallback((appointment, columnKey) => {
+    const cellValue = appointment[columnKey];
+    switch (columnKey) {
+      case "scheduleDate":
+        return formatFullDate(new Date(cellValue));
+      case "status":
+        switch (cellValue) {
+          case "CANCELLED":
+            return <Chip color="danger">Cancelled</Chip>;
+          case "CONFIRMED":
+            return <Chip color="primary">Confirmed</Chip>;
+          case "RESCHEDULED":
+            return <Chip color="secondary">Rescheduled</Chip>;
+          case "COMPLETED":
+            return <Chip color="success">Completed</Chip>;
+          default:
+            return <Chip color="warning">Pending</Chip>;
+        }
+      case "actions":
+        return (
+          <div className="flex items-center justify-center w-full">
+            {appointment.status === "COMPLETED" ||
+            appointment.status === "CANCELLED" ? (
+              <p>No Actions Available</p>
+            ) : (
+              <Tooltip
+                className="capitalize"
+                color="danger"
+                content="Cancel Appointment"
+              >
+                <i
+                  onClick={() => {
+                    cancelAppointmentById(appointment.id);
+                  }}
+                  className="fa-solid fa-trash text-red-600 cursor-pointer"
+                />
+              </Tooltip>
+            )}
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  return (
+    <div className="w-3/4 min-h-screen flex flex-col items-center gap-4 py-8 overflow-scroll">
+      <h1 className="text-ca-white text-3xl font-bold">
+        Your Appointments List
+      </h1>
+      <Table aria-label="Appointment Table" className=" w-4/5 text-black">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.label}</TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={appointmentList} emptyContent={"No rows to display."}>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- frontend/src/pages/patient/PatientPrescriptionsPage.jsx
+++ frontend/src/pages/patient/PatientPrescriptionsPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PatientSidebar from "../../components/patient/PatientSidebar";
+import MyPrescriptions from "../../components/patient/MyPrescriptions";
 
 export default function PatientPrescriptionsPage() {
   return (
     <div className="h-screen flex">
       <PatientSidebar />
+      <MyPrescriptions />
     </div>
   );
 }
