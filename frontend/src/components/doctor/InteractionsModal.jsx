236fd0056b4d20d9a7b3d53c9e7e8eb7047a53c6
Diego Lopez <diegolopez@meta.com>
2025-07-23 13:59:40 -0700

    Modal to show interactions modified to show AI interactions apart

--- frontend/src/components/doctor/InteractionsModal.jsx
+++ frontend/src/components/doctor/InteractionsModal.jsx
@@ -2,28 +2,42 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import checkDrugInteractions from "../../utils/checkDrugInteractions";
-import { Spinner, Divider } from "@heroui/react";
+import { Spinner, Divider, Button } from "@heroui/react";
+import { fetchInteractionsCall } from "../../api/interactions";
 
 export default function InteractionsModal({
   isOpen,
   onClose,
   prescriptionMedicineList,
+  selectedRecord,
 }) {
   if (!isOpen) return null;
 
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingAI, setIsLoadingAI] = useState(false);
   const [interactionsList, setInteractionsList] = useState([]);
+  const [aiInteractions, setAiInteractions] = useState([]);
 
   useEffect(() => {
     async function fetchDrugInteractions() {
       setIsLoading(true);
-      const newList = await checkDrugInteractions(prescriptionMedicineList);
-      setInteractionsList(newList);
+      const interactions = await fetchInteractionsCall(
+        selectedRecord,
+        prescriptionMedicineList
+      );
+      setInteractionsList(interactions);
       setIsLoading(false);
     }
 
     fetchDrugInteractions();
   }, []);
+
+  async function fetchAIDrugInteractions() {
+    setIsLoadingAI(true);
+    const newList = await checkDrugInteractions(prescriptionMedicineList);
+    setAiInteractions(newList);
+    setIsLoadingAI(false);
+  }
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
@@ -34,36 +48,89 @@ export default function InteractionsModal({
         >
           &times;
         </button>
+
+        {/* Symptoms and Drug Interactions */}
         {isLoading ? (
           <div className="flex flex-col items-center">
             <Spinner />
             <h3 className="mt-4 text-center">
-              The interactions between the prescription's current drugs are
-              loading...
+              The interactions between the prescription's current drugs and the
+              medical history symptoms are loading...
             </h3>
           </div>
         ) : (
           <>
-            <h2 className="text-xl font-semibold mb-4">Drug Interactions</h2>
+            <h2 className="text-xl font-semibold mb-4">
+              Drug / Symptoms Interactions
+            </h2>
 
             {interactionsList.length > 1 ? (
               <div>
                 {interactionsList.map((interaction, key) => (
                   <div key={key} className="text-gray-800">
-                    <h3 className="font-bold">{interaction.name}</h3>
-                    <p>{interaction.content}</p>
+                    <h3>
+                      <span className="font-bold">Medicine:</span>{" "}
+                      {interaction.medicine.brandName}{" "}
+                      <span className="text-red-500">x </span>
+                      <span className="font-bold">Symptom:</span>{" "}
+                      {interaction.symptom.name}
+                    </h3>
+                    <p>{interaction.description}</p>
                     <Divider className="my-4" />
                   </div>
                 ))}
               </div>
             ) : (
               <h3 className="text-ca-light-black text-center">
-                We couldn't find interactions between any pair of drugs in the
-                current prescription.
+                We couldn't find interactions between any pair of drug -
+                symptoms in the current prescription.
               </h3>
             )}
           </>
         )}
+        <h2 className="text-xl font-semibold mb-4">AI Drug Interactions</h2>
+        <div className="flex flex-col items-center justify-center">
+          {/* AI Between Drug Interaction */}
+          {isLoadingAI ? (
+            <div className="flex flex-col items-center">
+              <Spinner />
+              <h3 className="mt-4 text-center">
+                The interactions between the prescription's current drugs are
+                loading...
+              </h3>
+            </div>
+          ) : (
+            <>
+              {aiInteractions.length > 0 ? (
+                <div>
+                  {aiInteractions.map((interaction, key) => (
+                    <div key={key} className="text-gray-800">
+                      <h3 className="font-bold">{interaction.name}</h3>
+                      <p>{interaction.content}</p>
+                      <Divider className="my-4" />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div className="flex flex-col gap-2 justify-center">
+                  <h3 className="text-ca-light-black text-center">
+                    We couldn't find interactions between any pair of drugs in
+                    the current prescription.
+                  </h3>
+                </div>
+              )}
+              <Button
+                className="bg-ca-yellow my-2"
+                onPress={() => {
+                  fetchAIDrugInteractions();
+                }}
+                endContent={<i className="fa-solid fa-wand-magic-sparkles" />}
+              >
+                Get AI Interactions
+              </Button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
